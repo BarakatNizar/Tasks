a867e7ff5abd74237b958e9f0db990feda051eec
Nizar Barakat <nezarbarakat.nn@gmail.com>
2024-10-01 17:34:18 +0300

    Delete Task2Emailjs directory

--- Task2Emailjs/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./build/index.html' , './build/senders.html',"./build/js/*.js"], //All html file in build dir
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
